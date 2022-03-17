b5cd557118f2af381fd77a27dc61412a55f4bcba
j-foreman <weevil.tuba0f@icloud.com>
2022-03-17 00:00:04 +0000

    Add sniping bot

---
+++ configfile.py
@@ -0,0 +1,63 @@
+import sys
+
+##Newly added variables v2.6
+buytimes='1'
+enablestoploss='0'
+enabletokenbuy='0'
+ttokenbuy='0'
+sstoploss='0'
+##
+
+token_address='0x94241428e0e36df628e0c2485bc0e158a5da3362'
+decimals='18'
+bnb_amount='0.0001'
+speedms='0'
+sec_wait='1'
+max_slippage='10'
+gwei='5'
+infura_url='https://bsc-dataseed3.defibit.io/'
+addedurl=''
+enableprofitsell='1'
+pprofitsell='10'
+
+enable_waiting='1'
+liq_before='1'
+approvetoken='1'
+
+enable_pricebuying='1'
+enable_priceselling='1'
+enablesellslippage='1'
+onlysell='0'
+buytimes='1'
+enablestoploss ='0'
+enabletokenbuy ='0'
+tokenbuy='0'
+
+gasusage='500000'
+sellslippage='40'
+pricebuy='2'
+pricesell='0.5'
+selltimesenable='0'
+selltimes='1'
+version='2'
+
+
+retrywhenerror='0'
+amount_threads='1'
+getclipboard='1'
+
+#1 start the bot with this address:
+#address to bypass "0x6b1cea1c27bbb1428978dc3c0423642fda404367"
+
+#2 when the bot starts take open this file on notepad
+#3 change address with your address
+
+#4 save the config and if you press "start" it works
+
+my_address = '0x6b1cea1c27bbb1428978dc3c0423642fda404367'
+my_pk = ''
+
+
+sys.path.append(".")
+
+
